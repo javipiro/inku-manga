dab250c7ba3f9c22f76271b71f4ad96c33101457
javiera <ja.valdesg@duocuc.cl>
2026-04-14 13:55:12 -0400

    primera version  trabajo

---
+++ src/main/java/com/duoc/inkumanga/InkumangaApplication.java
@@ -0,0 +1,13 @@
+package com.duoc.inkumanga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InkumangaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InkumangaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/duoc/inkumanga/InkumangaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duoc.inkumanga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InkumangaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
